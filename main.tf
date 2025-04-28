3032688815e3a4255dd92547c8574401970f94fa
omar <omar.trigui@leminnov.com>
2025-04-28 12:15:48 +0200

    Create DNS record in scaleway

---
+++ main.tf
@@ -0,0 +1,12 @@
+resource "scaleway_domain_record" "this" {
+  for_each = {
+    for record in var.records : "${record.record_type}.${record.record_name}" => record
+  }
+
+  dns_zone = var.dns_zone
+
+  name     = each.value.record_name
+  type     = each.value.record_type
+  data     = each.value.record_data
+  ttl      = each.value.record_ttl
+}
---
+++ versions.tf
@@ -0,0 +1,12 @@
+terraform {
+     required_providers {
+         scaleway = {
+            source = "scaleway/scaleway"
+            version = "2.53.0" 
+            } 
+          }
+        }
+
+provider "scaleway" { 
+    # Configuration options 
+}
